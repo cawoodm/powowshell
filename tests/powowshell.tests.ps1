228b21aa249abbad232b10bc37fcfca7c1634012
Marc Cawood <498834+cawoodm@users.noreply.github.com>
2019-04-07 12:51:21 +0200

    Tests

--- tests/powowshell.tests.ps1
+++ tests/powowshell.tests.ps1
@@ -8,33 +8,28 @@ function main() {
     try {
         Push-Location $PSScriptRoot
 
-        Write-Host "------------------------CLEAN-----------------------------------------------------"
+        & ..\bin\workspace.ps1 ../examples | Out-Null
+        Write-Host "OK: pow workspace" -ForegroundColor Green
+
         & ..\bin\clean.ps1 ../examples/pipeline1 | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        Write-Host "OK: pow clean" -ForegroundColor Green
 
-        Write-Host "------------------------BUILD-----------------------------------------------------"
         & ..\bin\build.ps1 ../examples/pipeline1 | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        Write-Host "OK: pow build" -ForegroundColor Green
 
-        Write-Host "------------------------VERIFY----------------------------------------------------"
         & ..\bin\verify.ps1 ../examples/pipeline1 "@{DataSource='.\data\names.txt'}" | Out-Null
-        Write-Host "OK" -ForegroundColor Green
         & ..\bin\verify.ps1 ../examples/pipeline1 "DataSource=.\data\names.txt" | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        Write-Host "OK: pow verify" -ForegroundColor Green
 
-        Write-Host "------------------------RUN-------------------------------------------------------"
         & ..\bin\run.ps1 ../examples/pipeline1 -WhatIf @{DataSource='.\data\names.txt'} | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        Write-Host "OK: pow run" -ForegroundColor Green
 
-        Write-Host "------------------------HELP------------------------------------------------------"
         & ..\bin\help.ps1 verify | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        Write-Host "OK: pow help" -ForegroundColor Green
 
-        Write-Host "------------------------INSPECT---------------------------------------------------"
-        & ..\bin\inspect.ps1 ../examples/components/CSV2JSON.ps1 | Out-Null
-        Write-Host "OK" -ForegroundColor Green
+        & ..\bin\inspect.ps1 ..\examples\components\CSV2JSON.ps1 | Out-Null
+        Write-Host "OK: pow inspect" -ForegroundColor Green
 
-        Write-Host "----------------------------------------------------------------------------------"
     } catch {
         $Host.UI.WriteErrorLine("ERROR in $($_.InvocationInfo.ScriptName):$($_.InvocationInfo.ScriptLineNumber) : $($_.Exception.Message)")
         #throw $_
