d9ed4ffca5b790a2c0008c728a32db30a6cc678d
Marc <498834+cawoodm@users.noreply.github.com>
2019-05-03 17:03:27 +0200

    Autocomplete for cmdlet-list

--- ide/js/cmdlet-list.js
+++ ide/js/cmdlet-list.js
@@ -3,14 +3,17 @@ Vue.component("cmdlet-list", {
     props: [],
     data: function() {
         return {
-            filter: "",
+            filter: this.filter,
+            selectedCmdlet: null,
+            loading: this.loading,
             cmdlets: this.cmdlets
         };
     },
     computed: {
         filteredCmdlets() {
             if (this.cmdlets && this.filter) {
-                return this.cmdlets.filter((comp)=>comp.reference.indexOf(this.filter)>=0);
+                let filt = this.filter.toLowerCase();
+                return this.cmdlets.filter((comp)=>comp.reference.indexOf(filt)>=0);
             } else if (this.cmdlets) {
                 return this.cmdlets;
             } else {
@@ -21,18 +24,26 @@ Vue.component("cmdlet-list", {
     methods: {
         //showDialog: function(id) {},
         setCmdlets: function(cmdlets) {
+            this.loading = false;
             this.cmdlets = cmdlets;
+        },
+        addCmdlet: function() {
+            alert(this.selectedCmdlet)
         }
     },
-    updated: function() {},
+    mounted: function() {
+        console.log("mounted cmdlet-list")
+        this.filter = "";
+        this.loading = true;
+    },
     template: `
     <v-expansion-panel-content @hook:updated="$root.componentsUpdated">
-        <div slot="header">CmdLets</div>
-        <v-list dense>
+        <div slot="header" class="body-2">CmdLets</div>
+        <v-list dense style="height:80px; overflow: hidden">
             <v-list-tile class="search-tile">
-                <v-text-field v-model="filter" prepend-inner-icon="search"></v-text-field>
+                <v-autocomplete v-model="filter" :items="filteredCmdlets" item-text="name" label="CmdLets" clearable chips :loading="loading"></v-autocomplete>
             </v-list-tile>
-            <v-list-tile v-for="cmdlet in filteredCmdlets" class="drag cmdlet" :d-ref="cmdlet.reference" :key="cmdlet.reference">
+            <v-list-tile v-if="filter" v-for="cmdlet in filteredCmdlets" class="drag cmdlet nowrap1" :d-ref="cmdlet.reference" :key="cmdlet.reference">
             <v-icon>file_copy</v-icon>{{cmdlet.name}}
             </v-list-tile>
         </v-list>
