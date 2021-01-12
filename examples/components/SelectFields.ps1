2461e28875d6f00471f607e46f23aeb9960bdd5f
Marc Cawood <498834+cawoodm@users.noreply.github.com>
2021-01-12 18:25:16 +0100

    Correct example

--- examples/components/SelectFields.ps1
+++ examples/components/SelectFields.ps1
@@ -9,13 +9,13 @@
  psobject/any
 
  .Example
- .\SelectFields.ps1 '{"a": "foo", "b": 2, "c": 3}' | .\examples\components\SelectFields.ps1 -Fields "a", "b"
- Take JSON string '{a: "foo", b: 2, c: 3}' and return properties/fields "a" and "b" => '{a: "foo", b: 2}'
+ ConvertFrom-Json '{"a": "foo", "b": 2, "c": 3}' | .\SelectFields.ps1 -Fields "a", "b"
+ Take an object '{a: "foo", b: 2, c: 3}' and return properties/fields "a" and "b" => '{a: "foo", b: 2}'
 #>
 [OutputType([Object])]
 param(
-    [Parameter(ValueFromPipeline)][Object]$InputObject,
-    [String[]]$Fields
+  [Parameter(ValueFromPipeline)][Object]$InputObject,
+  [String[]]$Fields
 )
 
 $input | Select-Object -Property $Fields
