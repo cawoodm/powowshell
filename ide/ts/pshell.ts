ba553e32c31fe85d1ba451212856b23c58b5c847
Marc Cawood <498834+cawoodm@users.noreply.github.com>
2021-02-14 07:07:15 +0100

    Close and dispose

--- ide/ts/pshell.ts
+++ ide/ts/pshell.ts
@@ -4,46 +4,50 @@
  *  This also allows us to swap out the library later
  */
 type ShellOpts =  {
-    executionPolicy?: string;
-    noProfile?: boolean;
-    verbose?: boolean;
-    inputEncoding?: string;
-    outputEncoding?: string;
-    pwsh?: boolean
+  executionPolicy?: string;
+  noProfile?: boolean;
+  verbose?: boolean;
+  inputEncoding?: string;
+  outputEncoding?: string;
+  pwsh?: boolean
 }
 interface SearchFunc {
-    (source: string, subString: string): boolean;
+  (source: string, subString: string): boolean;
 }
 export function PShell() {
-    // ../ide/node_modules/node-powershell/lib/index.js
-    const shell = require("node-powershell");
-    let PowerShell;
-    return {
-        init: function(options: ShellOpts) {
-            PowerShell = new shell({
-                executionPolicy: options.executionPolicy || 'Bypass',
-                noProfile: options.noProfile || true,
-                verbose: options.verbose || false,
-                inputEncoding: options.inputEncoding || 'utf8',
-                outputEncoding: options.outputEncoding || 'utf8',
-                pwsh: options.pwsh || false
-            });
-            return PowerShell.pid;
-        },
-        exec: async function(command, args) {
-            return new Promise(function(resolve, reject) {
-                PowerShell.addCommand(command);
-                //args && PowerShell.addParameters(args);
-                //args && args.forEach(PowerShell.addArgument);
-                PowerShell.invoke()
-                    .then((output)=>{
-                        resolve({stdout: output, stderr: null})
-                    })
-                    .catch((err)=>{
-                        reject(err);
-                    });
-                //ps.on("error", reject);
-            })
-        }
+  // ../ide/node_modules/node-powershell/lib/index.js
+  const shell = require("node-powershell");
+  let PowerShell;
+  return {
+    init: function(options: ShellOpts) {
+      PowerShell = new shell({
+        executionPolicy: options.executionPolicy || 'Bypass',
+        noProfile: options.noProfile || true,
+        verbose: options.verbose || false,
+        inputEncoding: options.inputEncoding || 'utf8',
+        outputEncoding: options.outputEncoding || 'utf8',
+        pwsh: options.pwsh || false
+      });
+      return PowerShell.pid;
+    },
+    exec: async function(command, args) {
+      return new Promise(function(resolve, reject) {
+        PowerShell.addCommand(command);
+        //args && PowerShell.addParameters(args);
+        //args && args.forEach(PowerShell.addArgument);
+        PowerShell.invoke()
+          .then((output)=>{
+            resolve({stdout: output, stderr: null})
+          })
+          .catch((err)=>{
+            reject(err);
+          })
+          .finally(PowerShell.dispose)
+        //ps.on("error", reject);
+      })
+    },
+    close() {
+      PowerShell.dispose();
     }
+  }
 }
